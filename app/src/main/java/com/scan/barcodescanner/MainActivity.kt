f080808069c1b2f2073e1851162e49ee8d2f0d18
JanD4rk <kursat.mamedov@gmail.com>
2023-02-28 21:53:56 +0400

    demo

--- app/src/main/java/com/scan/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/scan/barcodescanner/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.scan.barcodescanner
 
 import android.Manifest
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Size
 import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ExperimentalGetImage
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
+import androidx.camera.camera2.internal.compat.workaround.TargetAspectRatio
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.scan.barcodescanner.databinding.ActivityMainBinding
 import java.util.concurrent.Executors
@@ -27,157 +28,245 @@ private const val CAMERA_PERMISSION_REQUEST_CODE = 1
 @ExperimentalGetImage
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  cameraReticleAnimator: CameraReticleAnimator
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-
-    if (hasCameraPermission()) bindCameraUseCases()
-    else requestPermission()
-  }
-
-  // checking to see whether user has already granted permission
-  private fun hasCameraPermission() =
-    ActivityCompat.checkSelfPermission(
-      this,
-      Manifest.permission.CAMERA
-    ) == PackageManager.PERMISSION_GRANTED
-
-  private fun requestPermission(){
-    // opening up dialog to ask for camera permission
-    ActivityCompat.requestPermissions(
-      this,
-      arrayOf(Manifest.permission.CAMERA),
-      CAMERA_PERMISSION_REQUEST_CODE
-    )
-  }
-
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<out String>,
-    grantResults: IntArray
-  ) {
-    if (requestCode == CAMERA_PERMISSION_REQUEST_CODE
-      && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      // user granted permissions - we can set up our scanner
-      bindCameraUseCases()
-    } else {
-      // user did not grant permissions - we can't use the camera
-      Toast.makeText(this,
-        "Camera permission required",
-        Toast.LENGTH_LONG
-      ).show()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        cameraReticleAnimator= CameraReticleAnimator(binding.overlay)
+        if (hasCameraPermission()) bindCameraUseCases()
+        else requestPermission()
     }
 
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-  }
+    // checking to see whether user has already granted permission
+    private fun hasCameraPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
 
-  private fun bindCameraUseCases() {
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-    cameraProviderFuture.addListener({
-      val cameraProvider = cameraProviderFuture.get()
-
-      // setting up the preview use case
-      val previewUseCase = Preview.Builder()
-        .build()
-        .also {
-          it.setSurfaceProvider(binding.cameraView.surfaceProvider)
-        }
-
-      // configure our MLKit BarcodeScanning client
-
-      /* passing in our desired barcode formats - MLKit supports additional formats outside of the
-      ones listed here, and you may not need to offer support for all of these. You should only
-      specify the ones you need */
-      val options = BarcodeScannerOptions.Builder().setBarcodeFormats(
-        Barcode.FORMAT_CODE_128,
-        Barcode.FORMAT_CODE_39,
-        Barcode.FORMAT_CODE_93,
-        Barcode.FORMAT_EAN_8,
-        Barcode.FORMAT_EAN_13,
-        Barcode.FORMAT_QR_CODE,
-        Barcode.FORMAT_UPC_A,
-        Barcode.FORMAT_UPC_E,
-        Barcode.FORMAT_PDF417
-      ).build()
-
-      // getClient() creates a new instance of the MLKit barcode scanner with the specified options
-      val scanner = BarcodeScanning.getClient(options)
-
-      // setting up the analysis use case
-      val analysisUseCase = ImageAnalysis.Builder()
-        .build()
-
-      // define the actual functionality of our analysis use case
-      analysisUseCase.setAnalyzer(
-        Executors.newSingleThreadExecutor(),
-        { imageProxy ->
-          processImageProxy(scanner, imageProxy)
-        }
-      )
-
-      // configure to use the back camera
-      val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-      try {
-        cameraProvider.bindToLifecycle(
-          this,
-          cameraSelector,
-          previewUseCase,
-          analysisUseCase)
-      } catch (illegalStateException: IllegalStateException) {
-        // If the use case has already been bound to another lifecycle or method is not called on main thread.
-        Log.e(TAG, illegalStateException.message.orEmpty())
-      } catch (illegalArgumentException: IllegalArgumentException) {
-        // If the provided camera selector is unable to resolve a camera to be used for the given use cases.
-        Log.e(TAG, illegalArgumentException.message.orEmpty())
-      }
-    }, ContextCompat.getMainExecutor(this))
-  }
-
-  private fun processImageProxy(
-    barcodeScanner: BarcodeScanner,
-    imageProxy: ImageProxy
-  ) {
-
-    imageProxy.image?.let { image ->
-      val inputImage =
-        InputImage.fromMediaImage(
-          image,
-          imageProxy.imageInfo.rotationDegrees
+    private fun requestPermission() {
+        // opening up dialog to ask for camera permission
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
         )
+    }
 
-      barcodeScanner.process(inputImage)
-        .addOnSuccessListener { barcodeList ->
-          val barcode = barcodeList.getOrNull(0)
-
-          // `rawValue` is the decoded value of the barcode
-          barcode?.rawValue?.let { value ->
-              binding.bottomText.text =
-                getString(R.string.barcode_value, value)
-          }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            // user granted permissions - we can set up our scanner
+            bindCameraUseCases()
+        } else {
+            // user did not grant permissions - we can't use the camera
+            Toast.makeText(
+                this,
+                "Camera permission required",
+                Toast.LENGTH_LONG
+            ).show()
         }
-        .addOnFailureListener {
-          // This failure will happen if the barcode scanning model
-          // fails to download from Google Play Services
 
-          Log.e(TAG, it.message.orEmpty())
-        }.addOnCompleteListener {
-          // When the image is from CameraX analysis use case, must
-          // call image.close() on received images when finished
-          // using them. Otherwise, new images may not be received
-          // or the camera may stall.
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
-          imageProxy.image?.close()
-          imageProxy.close()
+    private fun bindCameraUseCases() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // setting up the preview use case
+            val previewUseCase = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraView.surfaceProvider)
+
+                }
+
+            // configure our MLKit BarcodeScanning client
+
+            /* passing in our desired barcode formats - MLKit supports additional formats outside of the
+            ones listed here, and you may not need to offer support for all of these. You should only
+            specify the ones you need */
+            val options = BarcodeScannerOptions.Builder().setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+            ).build()
+
+            // getClient() creates a new instance of the MLKit barcode scanner with the specified options
+            val scanner = BarcodeScanning.getClient(options)
+
+            // setting up the analysis use case
+            val analysisUseCase = ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                .build()
+
+
+            // define the actual functionality of our analysis use case
+            analysisUseCase.setAnalyzer(Executors.newSingleThreadExecutor()) {
+                processImageProxy(scanner, it)
+            }
+
+
+            // configure to use the back camera
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+               var camera= cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    previewUseCase,
+                    analysisUseCase
+                )
+
+                binding.overlay.setCameraInfo(previewUseCase.resolutionInfo?.resolution)
+//                Log.e(TAG, "bindCameraUseCases3: ${previewUseCase.resolutionInfo} ${previewUseCase.resolutionInfo?.resolution}" )
+
+            } catch (illegalStateException: IllegalStateException) {
+                // If the use case has already been bound to another lifecycle or method is not called on main thread.
+                Log.e(TAG, illegalStateException.message.orEmpty())
+            } catch (illegalArgumentException: IllegalArgumentException) {
+                // If the provided camera selector is unable to resolve a camera to be used for the given use cases.
+                Log.e(TAG, illegalArgumentException.message.orEmpty())
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun processImageProxy(
+        barcodeScanner: BarcodeScanner,
+        imageProxy: ImageProxy
+    ) {
+
+        imageProxy.image?.let { image ->
+            val inputImage =
+                InputImage.fromMediaImage(
+                    image,
+                    imageProxy.imageInfo.rotationDegrees
+                )
+            barcodeScanner.process(inputImage)
+                .addOnSuccessListener { barcodeList ->
+                    val barcode = barcodeList.getOrNull(0)
+                    nese(binding.overlay,barcodeList)
+
+                    // `rawValue` is the decoded value of the barcode
+                    barcode?.rawValue?.let { value ->
+                        binding.bottomText.text =
+                            getString(R.string.barcode_value, value)
+                    }
+                }
+                .addOnFailureListener {
+                    // This failure will happen if the barcode scanning model
+                    // fails to download from Google Play Services
+
+                    Log.e(TAG, it.message.orEmpty())
+                }.addOnCompleteListener {
+                    // When the image is from CameraX analysis use case, must
+                    // call image.close() on received images when finished
+                    // using them. Otherwise, new images may not be received
+                    // or the camera may stall.
+
+                    imageProxy.image?.close()
+                    imageProxy.close()
+                }
         }
     }
-  }
 
-  companion object {
-    val TAG: String = MainActivity::class.java.simpleName
-  }
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+
+
+    fun nese(graphicOverlay:GraphicOverlay, results:List<Barcode>) {
+        Log.e(TAG, "processImageProxy: " + results.size)
+        val barcodeInCenter = results.firstOrNull { barcode ->
+            val boundingBox = barcode.boundingBox ?: return@firstOrNull false
+            val box = graphicOverlay.translateRect(boundingBox)
+            Log.e(TAG, "processImageProxy: box:${box}")
+
+            Log.e(TAG, "processImageProxy: x:${graphicOverlay.width /2f} y:${graphicOverlay.height/2f}")
+            box.contains(graphicOverlay.width / 2f, graphicOverlay.height / 2f)
+        }
+        Log.e(TAG, "processImageProxy2:  ${barcodeInCenter == null}")
+
+
+        graphicOverlay.clear()
+        if (barcodeInCenter == null) {
+            cameraReticleAnimator.start()
+            graphicOverlay.add(BarcodeReticleGraphic(graphicOverlay, cameraReticleAnimator))
+//            workflowModel.setWorkflowState(WorkflowState.DETECTING)
+        } else {
+            cameraReticleAnimator.cancel()
+            val sizeProgress = getProgressToMeetBarcodeSizeRequirement(
+                graphicOverlay,
+                barcodeInCenter
+            )
+            if (sizeProgress < 1) {
+                // Barcode in the camera view is too small, so prompt user to move camera closer.
+                graphicOverlay.add(BarcodeConfirmingGraphic(graphicOverlay, barcodeInCenter))
+//                workflowModel.setWorkflowState(WorkflowState.CONFIRMING)
+            } else {
+                // Barcode size in the camera view is sufficient.
+                if (true) {
+                    val loadingAnimator = createLoadingAnimator(graphicOverlay)
+                    loadingAnimator.start()
+                    graphicOverlay.add(BarcodeLoadingGraphic(graphicOverlay, loadingAnimator))
+//                    workflowModel.setWorkflowState(WorkflowState.SEARCHING)
+                } else {
+//                    workflowModel.setWorkflowState(WorkflowState.DETECTED)
+//                    workflowModel.detectedBarcode.setValue(barcodeInCenter)
+                }
+            }
+        }
+        graphicOverlay.invalidate()
+    }
+
+    private fun createLoadingAnimator(graphicOverlay: GraphicOverlay): ValueAnimator {
+        val endProgress = 1.1f
+        return ValueAnimator.ofFloat(0f, endProgress).apply {
+            duration = 2000
+            addUpdateListener {
+                if ((animatedValue as Float).compareTo(endProgress) >= 0) {
+                    graphicOverlay.clear()
+//                    workflowModel.setWorkflowState(WorkflowState.SEARCHED)
+//                    workflowModel.detectedBarcode.setValue(barcode)
+                } else {
+                    graphicOverlay.invalidate()
+                }
+            }
+        }
+    }
+
+    fun getProgressToMeetBarcodeSizeRequirement(overlay: GraphicOverlay, barcode: Barcode): Float {
+        val context = overlay.context
+        return if (false) {
+            val reticleBoxWidth = getBarcodeReticleBox(overlay).width()
+            val barcodeWidth = overlay.translateX(barcode.boundingBox?.width()?.toFloat() ?: 0f)
+            val requiredWidth =
+                reticleBoxWidth * 50 / 100
+            (barcodeWidth / requiredWidth).coerceAtMost(1f)
+        } else {
+            1f
+        }
+    }
+
+    fun getBarcodeReticleBox(overlay: GraphicOverlay): RectF {
+        val context = overlay.context
+        val overlayWidth = overlay.width.toFloat()
+        val overlayHeight = overlay.height.toFloat()
+        val boxWidth =
+            overlayWidth * 80/ 100
+        val boxHeight =
+            overlayHeight * 35 / 100
+        val cx = overlayWidth / 2
+        val cy = overlayHeight / 2
+        return RectF(cx - boxWidth / 2, cy - boxHeight / 2, cx + boxWidth / 2, cy + boxHeight / 2)
+    }
 }
